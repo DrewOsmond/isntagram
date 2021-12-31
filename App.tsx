5e42535d4da95e3c85789a36fc7824d11e9309f5
drewosmond <drewosmond@gmail.com>
2021-12-30 19:07:18 -0800

    updated firebase

--- App.tsx
+++ App.tsx
@@ -1,19 +1,13 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
+// import { initializeApp } from "firebase/app";
+// import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
 import firebase from "@firebase/app";
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBZFy352HG68NgWIvq5Qd5w2wReNx5jGjM",
-//   authDomain: "isntagram-23f77.firebaseapp.com",
-//   projectId: "isntagram-23f77",
-//   storageBucket: "isntagram-23f77.appspot.com",
-//   messagingSenderId: "831791431090",
-//   appId: "1:831791431090:web:06f8805002e036f3b038ab",
-//   measurementId: "G-0W2ZZQL3VX",
-// };
-// firebase.auth().createUserWithEmailAndPassword(email, passowrd);
+import firebaseConfig from "./firebaseConfig";
+console.log(firebase);
+if (firebase.getApps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 import LandingScreen from "./src/screens/landing/Landing";
 import RegisterScreen from "./src/screens/register/Register";
